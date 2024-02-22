09ee70516216cf44c2084823dd9e4940b1557775
laichunyin <laichunyin@gmail.com>
2024-02-22 00:08:19 -0500

    Initial Commit

---
+++ tracker_ios_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tracker_ios_app
+//
+//  Created by macbook on 22/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tracker_ios_app/tracker_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracker_ios_appApp.swift
+//  tracker_ios_app
+//
+//  Created by macbook on 22/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct tracker_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
